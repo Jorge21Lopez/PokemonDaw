3df47d2042415c8c7a9adb68d673ec87f494b629
Marcos López Llorente <167746183+Sickospren@users.noreply.github.com>
2024-10-16 19:20:40 +0200

    Create pokedex.php

---
+++ pokedex.php
@@ -0,0 +1,76 @@
+<?php
+
+$garchomp = [   
+                "name"=>"garchomp",
+                "hp"=>108,
+                "ataque"=>130,
+                "def"=>95,
+                "spe"=>102,
+                "img"=>"<a href='https://pokemondb.net/pokedex/garchomp'><img src='https://img.pokemondb.net/sprites/black-white/normal/garchomp.png' alt='Garchomp'></a>",
+            ];
+
+$charizard = [
+                "name"=>"charizard",
+                "hp"=>78,
+                "ataque"=>84,
+                "def"=>78,
+                "spe"=>100,
+                "img"=>"<a href='https://pokemondb.net/pokedex/charizard'><img src='https://img.pokemondb.net/sprites/black-white/normal/charizard.png' alt='Charizard'></a>",
+            ];
+
+$typhlosion = [
+                "name"=>"typhlosion",
+                "hp"=>78,
+                "ataque"=>84,
+                "def"=>78,
+                "spe"=>100,
+                "img"=>"<a href='https://pokemondb.net/pokedex/typhlosion'><img src='https://img.pokemondb.net/sprites/black-white/normal/typhlosion.png' alt='Typhlosion'></a>",
+            ];
+
+$umbreon = [
+                "name"=>"umbreon",
+                "hp"=>95,
+                "ataque"=>65,
+                "def"=>110,
+                "spe"=>65,
+                "img"=>"<a href='https://pokemondb.net/pokedex/umbreon'><img src='https://img.pokemondb.net/sprites/black-white/normal/umbreon.png' alt='Umbreon'></a>",
+            ];
+
+$vaporeon = [
+                "name"=>"vaporeon",
+                "hp"=>130,
+                "ataque"=>65,
+                "def"=>60,
+                "spe"=>65,
+                "img"=>"<a href='https://pokemondb.net/pokedex/vaporeon'><img src='https://img.pokemondb.net/sprites/black-white/normal/vaporeon.png' alt='Vaporeon'></a>",
+            ];
+
+$gengar = [
+                "name"=>"gengar",
+                "hp"=>60,
+                "ataque"=>130,
+                "def"=>60,
+                "spe"=>110,
+                "img"=>"<a href='https://pokemondb.net/pokedex/gengar'><img src='https://img.pokemondb.net/sprites/black-white/normal/gengar.png' alt='Gengar'></a>",
+            ];
+
+$hydreigon = [
+                "name"=>"hydreigon",
+                "hp"=>92,
+                "ataque"=>105,
+                "def"=>90,
+                "spe"=>98,
+                "img"=>"<a href='https://pokemondb.net/pokedex/hydreigon'><img src='https://img.pokemondb.net/sprites/black-white/anim/normal/hydreigon.gif' alt='Hydreigon'></a>",
+            ];
+
+$crobat = [
+                "name"=>"crobat",
+                "hp"=>85,
+                "ataque"=>90,
+                "def"=>80,
+                "spe"=>130,
+                "img"=>"<a href='https://pokemondb.net/pokedex/crobat'><img src='https://img.pokemondb.net/sprites/black-white/anim/normal/crobat.gif' alt='Crobat'></a>",
+            ];
+
+
+$pokedex = [$garchomp,$charizard,$typhlosion,$umbreon,$vaporeon,$gengar,$hydreigon,$crobat];
